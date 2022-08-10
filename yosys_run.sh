61a4b84ccca07e753015ce25ba8cab0198bb451b
AjayKumar-Gandi <110395336+AjayKumar-Gandi@users.noreply.github.com>
2022-08-10 20:53:49 +0530

    Update yosys_run.sh

--- yosys_run.sh
+++ yosys_run.sh
@@ -5,8 +5,8 @@ read_verilog iiitb_SDM.v
 # generic synthesis
 synth -top iiitb_SDM
 # mapping to mycells.lib
-dfflibmap -liberty /home/ajaykumar/iiitb_SDM/lib/sky130_fd_sc_hd__tt_025C_1v80.lib
-abc -liberty /home/ajaykumar/iiitb_SDM/lib/sky130_fd_sc_hd__tt_025C_1v80.lib -script +strash;scorr;ifraig;retime,{D};strash;dch,-f;map,-M,1,{D}
+dfflibmap -liberty /home/ajaykumar/iiitb_SDM_1010/lib/sky130_fd_sc_hd__tt_025C_1v80.lib
+abc -liberty /home/ajaykumar/iiitb_SDM_1010/lib/sky130_fd_sc_hd__tt_025C_1v80.lib -script +strash;scorr;ifraig;retime,{D};strash;dch,-f;map,-M,1,{D}
 clean
 flatten
 # write synthesized design
