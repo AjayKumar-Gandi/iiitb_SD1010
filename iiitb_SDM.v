b03f27c2459af3524e27fc6ccd6fe3d78c786461
AjayKumar-Gandi <110395336+AjayKumar-Gandi@users.noreply.github.com>
2022-08-16 21:45:29 +0530

    Update iiitb_SDM.v

--- iiitb_SDM.v
+++ iiitb_SDM.v
@@ -3,19 +3,26 @@ input din;
 input clk;
 input reset;
 output reg y;
-reg [2:0] cst, nst;
+reg [1:0] cst, nst;
 parameter S0 = 2'b00,
           S1 = 2'b01,
           S2 = 2'b10,
           S3 = 2'b11;
-always@(posedge clk, posedge reset)
+always@(posedge clk or posedge reset)
 begin
            if (reset==1)
              cst <= S0;
            else
              cst <= nst;
-end          
-always @(cst , din)
+end   
+always @ (posedge clk) begin
+    if (reset==1) y <= 1'b0;
+    else begin
+      if (~din & (cst == S3)) y <= 1'b1;
+      else y <= 1'b0;
+    end
+end       
+always @(cst or din)
  begin
  case (cst)
    S0:begin 
@@ -44,12 +51,5 @@ always @(cst , din)
        end
    default: nst <= S0;
   endcase
-end
-always @ (posedge clk) begin
-    if (reset==1) y <= 1'b0;
-    else begin
-      if (~din & (cst == S3)) y <= 1'b1;
-      else y <= 1'b0;
-    end
 end
 endmodule
