8a0955a9e67b010889853aeb853f4248d71f5fde
AjayKumar-Gandi <110395336+AjayKumar-Gandi@users.noreply.github.com>
2022-08-31 20:18:02 +0530

    Update iiitb_SDM_tb.v

--- iiitb_SDM_tb.v
+++ iiitb_SDM_tb.v
@@ -1,31 +1,31 @@
 module iiitb_SDM_tb;
-reg din,clk,reset;
-wire y;
-iiitb_SDM m1(din, reset, clk, y);
+reg sequence_in,clock,reset;
+wire detector_out;
+iiitb_SDM m1(sequence_in,clock,reset,detector_out);
 initial
 begin
 $dumpfile("sqnsdet_tb.vcd");
 $dumpvars(0);
-clk=0;din=0;
+clock=0;sequence_in=0;
 reset=1;
-#10 reset=0;
-$monitor($time, , ,"c=%b",clk,,"y=%b",y,,"r=%b",reset,,"d=%b",din);
-#10 din=0;
-#10 din=1;
-#10 din=0;
-#10 din=1;
-#10 din=0;
-#10 din=1;
-#10 din=0;
-#10 din=0;
-#10 din=1;
-#10 din=0;
-#10 din=0;
-//#5 reset=1;
-//#5 reset=0;
+#10 reset=1;
+#10 reset =0;
+$monitor($time, , ,"c=%b",clock,,"y=%b",detector_out,,"r=%b",reset,,"d=%b",sequence_in);
+#10 sequence_in=0;
+#10 sequence_in=1;
+#10 sequence_in=0;
+#10 sequence_in=1;
+#10 sequence_in=0;
+#10 sequence_in=1;
+#10 sequence_in=1;
+#10 sequence_in=0;
+#10 sequence_in=1;
+#10 sequence_in=0;
+#10 sequence_in=0;
+
 end
 always
-#5 clk=~clk;
+#5 clock=~clock;
 initial
-#100 $finish ;
+#200 $finish ;
 endmodule
